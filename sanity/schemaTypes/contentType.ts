0c34b6b8c4fc6c31d4be9ba76365884a634e0ec3
josh-frost <frost.joshe@gmail.com>
2026-01-30 08:49:46 -0600

    feat: add contentType

---
+++ sanity/schemaTypes/contentType.ts
@@ -0,0 +1,38 @@
+import { defineField, defineType } from "sanity";
+
+export const contentType = defineType({
+  name: "content",
+  title: "Content",
+  type: "document",
+  fields: [
+    defineField({
+      name: "page",
+      title: "Page",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "body",
+      title: "Body",
+      type: "array",
+      of: [{ type: "block" }],
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+});
